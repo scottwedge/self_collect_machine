8e014168fc9fdd4d13c66aa71e3aacc9249c8cbb
khairulizwan <wansnap@gmail.com>
2020-05-21 09:27:13 +0800

    Updated on 05-21-2020-09:22:07 from 172.20.10.2 : khairulizwan-MacBookPro

---
+++ script/box_validity_rev2.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+################################################################################
+## {Description}: Validate the QR/Bar Code (for USB type camera)
+################################################################################
+## Author: Khairul Izwan Bin Kamsani
+## Version: {1}.{0}.{0}
+## Email: {wansnap@gmail.com}
+################################################################################
+
+from __future__ import print_function
+from __future__ import division
+
+import sys
+import rospy
+
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from self_collect_machine.msg import boxStatus
+
+import csv
+import datetime
+import cv2
+
+class BoxIDValidate_node:
+	def __init__(self):
+		# Initializing your ROS Node
+		rospy.init_node('BoxIDValidate_node', anonymous=True)
+
+		rospy.on_shutdown(self.shutdown)
+
+		# Subscribe to the sensorState_1 topic
+		self.sensorState1_sub = rospy.Subscriber("/sensorState_1", Int32)
+
+		# Subscribe to the sensorState_2 topic
+		self.sensorState2_sub = rospy.Subscriber("/sensorState_2", Int32)
+
+		# Subscribe to the sensorState_3 topic
+#		self.sensorState3_sub = rospy.Subscriber("/sensorState_3", Int32)
+
+		# TODO: may add more
+		# Subscribe to the sensorState_N topic
+		#self.sensorStateN_sub = rospy.Subscriber("/sensorState_N", Int32)
+
+		# Subscribe to the boxIDstate_1 topic
+		self.boxIDstate1_sub = rospy.Subscriber("/switchState_1", Int32)
+
+		# Publish to the scanned_barcode topic
+		self.boxStatus_pub = rospy.Publisher("/box_available", boxStatus, queue_size=10)
+
+		# Publish to the scanned_barcode topic
+		self.boxIDStatus_pub = rospy.Publisher("/box_door", boxStatus, queue_size=10)
+
+		self.getBoxState()
+
+	def getSensorState1(self):
+		# Wait for the topic
+		self.state1 = rospy.wait_for_message("/sensorState_1", Int32)
+
+	def getSensorState2(self):
+		# Wait for the topic
+		self.state2 = rospy.wait_for_message("/sensorState_2", Int32)
+
+#	def getSensorState3(self):
+#		# Wait for the topic
+#		self.state3 = rospy.wait_for_message("/sensorState_3", Int32)
+
+	# TODO: may add more
+	#def getSensorStateN(self):
+		# Wait for the topic
+		#self.stateN = rospy.wait_for_message("/sensorState_N", Int32)
+
+	def getBoxIDstate1(self):
+		# Wait for the topic
+		self.boxIDstate1 = rospy.wait_for_message("/switchState_1", Int32)
+
+	# Shutdown
+	def shutdown(self):
+		try:
+			rospy.loginfo("[INFO] BoxIDValidate_node [OFFLINE]...")
+
+		finally:
+			pass
+
+	def getBoxState(self):
+		# Initiate the topic
+		self.boxState = boxStatus()
+		self.boxIDState = boxStatus()
+
+		while not rospy.is_shutdown():
+			# Get the scan-ed data
+			self.getSensorState1()
+			self.getSensorState2()
+#			self.getSensorState3()
+			# TODO: May add more here
+
+			self.getBoxIDstate1()
+
+			self.boxState.data = [self.state1.data, self.state2.data]
+			self.boxStatus_pub.publish(self.boxState)
+
+			self.boxIDState.data = [self.boxIDstate1.data]
+			self.boxIDStatus_pub.publish(self.boxIDState)
+
+def main(args):
+	vn = BoxIDValidate_node()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		rospy.loginfo("[INFO] BoxIDValidate_node [OFFLINE]...")
+
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	rospy.loginfo("[INFO] BoxIDValidate_node [ONLINE]...")
+	main(sys.argv)
---
+++ script/store_barcode_record_rev3.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+
+################################################################################
+## {Description}: Record the QR/Bar Code on CustomerDatabase and Sent Email
+################################################################################
+## Author: Khairul Izwan Bin Kamsani
+## Version: {1}.{0}.{0}
+## Email: {wansnap@gmail.com}
+################################################################################
+
+from __future__ import print_function
+from __future__ import division
+
+import sys
+import rospy
+import cv2
+import imutils
+
+from std_msgs.msg import String
+from self_collect_machine.msg import boxStatus
+
+from pyzbar import pyzbar
+import datetime
+import time
+import os
+import rospkg
+import numpy as np
+
+import pyqrcode
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+class StoreBarcodeRecord_node:
+	def __init__(self):
+		# Initializing your ROS Node
+		rospy.init_node('StoreBarcodeRecord_node', anonymous=True)
+
+		rospy.on_shutdown(self.shutdown)
+
+		# initialize the output directory path and create the output
+		# directory
+		rospy.logwarn("Create an output folder")
+
+		self.rospack = rospkg.RosPack()
+
+		self.p = os.path.sep.join([self.rospack.get_path('self_collect_machine')])
+
+		self.outputDir = os.path.join(self.p, "csv")
+		self.csv_filename = self.outputDir + "/store_barcode" + ".csv"
+		self.csv = open(self.csv_filename, "a")
+		self.found = set()
+
+		self.outputQRDir = os.path.join(self.p, "qr_code")
+
+		# Subscribe to the scanned_barcode topic
+		self.scannedBar_sub = rospy.Subscriber("/scanned_barcode", String)
+
+		# Subscribe to the scan_mode topic
+		self.scanMode_sub = rospy.Subscriber("/scan_mode", String)
+
+		# Subscribe to the box_available topic
+		self.boxStatus_sub = rospy.Subscriber("/box_available", boxStatus)
+
+		# Subscribe to the box_available topic
+		self.boxDoor_sub = rospy.Subscriber("/box_door", boxStatus)
+
+		# TODO
+		# Publish to the scan_status topic
+		#self.scanStatus_pub = rospy.Publisher("/scan_status", String, queue_size=10)
+
+		# Publish to the box_available topic
+		self.boxDoor_pub = rospy.Publisher("/box_door_status", boxStatus)
+
+		self.storeRecord()
+
+	# Get scanned barcode
+	def getQR(self):
+		# Wait for the topic
+		self.qr = rospy.wait_for_message("/scanned_barcode", String)
+
+	# Get scanned barcode
+	def getMode(self):
+		# Wait for the topic
+		self.mode = rospy.wait_for_message("/scan_mode", String)
+
+	# Get scanned barcode
+	def getBox(self):
+		# Wait for the topic
+		self.box = rospy.wait_for_message("/box_available", boxStatus)
+
+	# Get scanned barcode
+	def getBoxDoor(self):
+		# Wait for the topic
+		self.boxDoor = rospy.wait_for_message("/box_door", boxStatus)
+
+	# Shutdown
+	def shutdown(self):
+		try:
+			rospy.loginfo("[INFO] StoreBarcodeRecord_node [OFFLINE]...")
+
+		finally:
+			pass
+
+	def storeRecord(self):
+		# TODO
+		# Initiate the topic
+		#self.scanStatus = String()
+
+		while not rospy.is_shutdown():
+			# Get the scan-ed data
+			self.getQR()
+			self.getMode()
+			self.getBox()
+			self.getBoxDoor()
+
+			self.boxID = np.array(self.box.data)
+			self.boxID = np.where(self.boxID == 0)[0]
+
+			#rospy.loginfo(self.scanMode)
+			if self.mode.data == "store":
+				# TODO: Un-comment for troubleshoot
+				#rospy.loginfo(self.qr.data)
+
+				if len(self.boxID) > 0:
+					# TODO: Un-comment for troubleshoot
+					#rospy.logwarn("Empty Box Available")
+
+					# if the barcode text is currently not in our CSV file, write
+					# the timestamp + barcode to disk and update the set
+					if self.qr.data not in self.found:
+						# TODO: Publish the data for opening the locker door
+						
+						self.csv.write("{},{},{}\n".format(datetime.datetime.now(), self.qr.data, self.boxID[0]))
+						# TODO: Un-comment for troubleshoot
+						rospy.logwarn("Saved as: {},{},{}\n".format(datetime.datetime.now(), self.qr.data, self.boxID[0]))
+						self.csv.flush()
+						self.found.add(self.qr.data)
+
+						# Generate QR Code
+						self.custQR = self.qr.data.rsplit(',', 1)[0]
+						self.urlcustomer = pyqrcode.create(self.custQR)
+
+						self.customerQR = self.outputQRDir + "/" + self.custQR + ".png"
+
+						self.urlcustomer.png(self.customerQR, scale=8)
+
+						# Email QR Code
+						email = 'wansnap@gmail.com' # Your email
+						password = 'Kh@irulizwan1984' # Your email account password
+						send_to_emails = [self.qr.data.rsplit(',', 1)[1], 'shafikahdarwis@gmail.com'] # List of email addresses
+						subject = "Order ID " + self.custQR # The subject line
+						message = "Please proceed to our Self Collect Machine, and show the Code" # The message in the email
+						file_location = self.customerQR
+
+						# Create the attachment file (only do it once)
+						filename = os.path.basename(file_location)
+						attachment = open(file_location, "rb")
+						part = MIMEBase('application', 'octet-stream')
+						part.set_payload(attachment.read())
+						encoders.encode_base64(part)
+						part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+						# Connect and login to the email server
+						server = smtplib.SMTP('smtp.gmail.com', 587)
+						server.starttls()
+						server.login(email, password)
+
+						# Loop over each email to send to
+						for send_to_email in send_to_emails:
+							# Setup MIMEMultipart for each email address (if we don't do this, the emails will concat on each email sent)
+							msg = MIMEMultipart()
+							msg['From'] = email
+							msg['To'] = send_to_email
+							msg['Subject'] = subject
+
+							# Attach the message to the MIMEMultipart object
+							msg.attach(MIMEText(message, 'plain'))
+							# Attach the attachment file
+							msg.attach(part)
+
+							# Send the email to this specific email address
+							server.sendmail(email, send_to_email, msg.as_string()) 
+
+						# Quit the email server when everything is done
+						rospy.logwarn("Detail has been emailed")
+						server.quit()
+
+					else:
+						rospy.logerr("IN Record!")
+
+				else:
+					rospy.logerr("No Empty Box Available")
+
+def main(args):
+	vn = StoreBarcodeRecord_node()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		rospy.loginfo("[INFO] StoreBarcodeRecord_node [OFFLINE]...")
+
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	rospy.loginfo("[INFO] StoreBarcodeRecord_node [ONLINE]...")
+	main(sys.argv)
