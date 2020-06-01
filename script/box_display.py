dd69d5064856f7d9d829fc5abcbce3703b1a5570
khairulizwan <wansnap@gmail.com>
2020-06-01 12:08:38 +0800

    Updated on 06-01-2020-09:37:16 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/box_display.py
+++ script/box_display.py
@@ -55,12 +55,10 @@ class BoxIDDisplay_node:
 			show_message(self.device, 'BoxID: {}'.format(self.sensor.data), fill="white", font=proportional(LCD_FONT), scroll_delay=0.08)
 
 			with canvas(self.virtual) as draw:
-				text(draw, (1, 1), "BoxID: {}".format(self.sensor.data), fill="white", font=proportional(CP437_FONT))
+				text(draw, (1, 1), "BoxID: {}".format(self.sensor.data), fill="white", font=proportional(LCD_FONT))
 
 			self.sensor_val = self.sensor.data
 		else:
-#			show_message(self.device, 'Welcome to AUTOBOTIC Self Collect Machine', 
-#				fill="white", font=proportional(LCD_FONT), scroll_delay=0.05)
 			pass
 
 if __name__ == '__main__':
